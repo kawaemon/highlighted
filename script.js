2e8fb0b2310d6d7b4536743d09767f1b9a541fd0
kawaemon <me@kawaemon.dev>
2025-04-07 11:35:01 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const el = (x) => document.getElementById(x);
-const inputs = [el("langs"), el("code")];
+const inputs = [el("langs"), el("code"), el("css")];
 const copybutton = el("copybutton");
 const output = el("output");
-const [langSel, code] = inputs;
+const [langSel, code, css] = inputs;
 
 // prettier-ignore
 const languageList = ["plaintext", "html", "1c", "abnf", "accesslog", "actionscript", "ada", "angelscript", "apache", "applescript", "arcade", "arduino", "armasm", "asciidoc", "aspectj", "autohotkey", "autoit", "avrasm", "awk", "axapta", "bash", "basic", "bnf", "brainfuck", "c", "cal", "capnproto", "ceylon", "clean", "clojure-repl", "clojure", "cmake", "coffeescript", "coq", "cos", "cpp", "crmsh", "crystal", "csharp", "csp", "css", "d", "dart", "delphi", "diff", "django", "dns", "dockerfile", "dos", "dsconfig", "dts", "dust", "ebnf", "elixir", "elm", "erb", "erlang-repl", "erlang", "excel", "fix", "flix", "fortran", "fsharp", "gams", "gauss", "gcode", "gherkin", "glsl", "gml", "go", "golo", "gradle", "graphql", "groovy", "haml", "handlebars", "haskell", "haxe", "hsp", "http", "hy", "inform7", "ini", "irpf90", "isbl", "java", "javascript", "jboss-cli", "json", "julia-repl", "julia", "kotlin", "lasso", "latex", "ldif", "leaf", "less", "lisp", "livecodeserver", "livescript", "llvm", "lsl", "lua", "makefile", "markdown", "mathematica", "matlab", "maxima", "mel", "mercury", "mipsasm", "mizar", "mojolicious", "monkey", "moonscript", "n1ql", "nestedtext", "nginx", "nim", "nix", "node-repl", "nsis", "objectivec", "ocaml", "openscad", "oxygene", "parser3", "perl", "pf", "pgsql", "php-template", "php", "plaintext", "pony", "powershell", "processing", "profile", "prolog", "properties", "protobuf", "puppet", "purebasic", "python-repl", "python", "q", "qml", "r", "reasonml", "rib", "roboconf", "routeros", "rsl", "ruby", "ruleslanguage", "rust", "sas", "scala", "scheme", "scilab", "scss", "shell", "smali", "smalltalk", "sml", "sqf", "sql", "stan", "stata", "step21", "stylus", "subunit", "swift", "taggerscript", "tap", "tcl", "thrift", "tp", "twig", "typescript", "vala", "vbnet", "vbscript-html", "vbscript", "verilog", "vhdl", "vim", "wasm", "wren", "x86asm", "xl", "xml", "xquery", "yaml", "zephir"];
@@ -23,6 +23,23 @@ let themecss = "";
   themecss = await data.text();
   regen();
 })();
+
+const astyle = `\
+pre code.hljs {
+    padding: 1.5em 1em;
+}
+.hljs {
+    border-radius: 4px;
+    background: #f7f7f7;
+    line-height: 1.2;
+    font-size: 16px;
+}
+@media screen and (max-width: 540px) {
+    .hljs {
+        font-size: 15px;
+    }
+}`;
+css.value = astyle;
 
 function escapeHtml(unsafe) {
   return unsafe
@@ -45,6 +62,7 @@ copybutton.addEventListener("click", () => {
   <body>
     ${targethtml}
     <style>${themecss}</style>
+    <style>${css.value}</style>
   </body>
 </html>`;
 
@@ -71,6 +89,7 @@ function regen() {
       <pre id="target"><code class="language-${langSel.value}">${escapeHtml(code.value)}</code></pre>
     </div>
     <style>${themecss}</style>
+    <style>${css.value}</style>
     <script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@latest/build/highlight.min.js"></script>
     <script>hljs.highlightAll();</script>
   </body>
